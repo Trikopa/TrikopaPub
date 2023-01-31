362310cf764626fed81e6c72ec997c3439a8e670
Trikopa <119809586+Trikopa@users.noreply.github.com>
2023-01-31 17:00:32 +0100

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,75 +1,135 @@
-package org.hyperskill.secretdiary
+package org.hyperskill.stopwatch
 
-import android.app.AlertDialog
+import android.app.*
 import android.content.Context
-import android.content.SharedPreferences
-import android.icu.text.SimpleDateFormat
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.net.Uri.Builder
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.os.Handler
+import android.os.Looper
+import android.text.InputType
+import android.view.View
 import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
-const val PREFERENCES_NAME = "PREF_DIARY"
+
 class MainActivity : AppCompatActivity() {
-    lateinit var sharedPreferences: SharedPreferences
-
-       override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sharedPreferences = getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        val saveBtn = findViewById<Button>(R.id.btnSave)
-        val editText = findViewById<EditText>(R.id.etNewWriting)
-        val savedDiary = findViewById<TextView>(R.id.tvDiary)
-        val alertDialogButton = findViewById<Button>(R.id.btnUndo)
-        var messages = mutableListOf<String>()
-        savedDiary.text = sharedPreferences.getString("KEY_DIARY_TEXT","")
-        saveBtn.setOnClickListener {
-            val time = kotlinx.datetime.Clock.System.now().toEpochMilliseconds()
-            val timeNow  = SimpleDateFormat("yyyy-MM-dd HH:mm:ss",Locale.getDefault()).format(time).toString()
-            val msg = editText.text.toString()
-            if(msg.isBlank()) {
-                Toast.makeText(applicationContext, "Empty or blank input cannot be saved", Toast.LENGTH_SHORT).show()
-            } else {
-                val lastSaved = savedDiary.text
-                savedDiary.text = "$timeNow\n${editText.text}"
-                messages.add(0,savedDiary.text.toString())
-                if(lastSaved.isNotEmpty()) {
-                    savedDiary.append("\n\n")
-                }
-                savedDiary.append(lastSaved)
-                editText.text.clear()
-                editor.putString("KEY_DIARY_TEXT",savedDiary.text.toString()).apply()
+        val handler = Handler(Looper.getMainLooper())
+        var timeOfStart = System.currentTimeMillis()
+        var timerRunning = false
+        var alertTime = 100000.toLong()
+        var notNotified = true
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val channelID = "org.hyperskill"
+        val name = "Delivery status"
+        val descriptionText = "Your delivery status"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(channelID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+        val notificationBuilder = NotificationCompat.Builder(applicationContext, channelID )
+            .setSmallIcon(R.drawable.progress)
+            .setContentTitle("Delivery")
+            .setContentText("Food is ready!")
+            .setContentIntent(pendingIntent)
+            .setStyle(NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setTimeoutAfter(700)
+        val notification: Notification = notificationBuilder.build()
+        notification.flags = Notification.FLAG_INSISTENT or Notification.FLAG_ONLY_ALERT_ONCE
+        val changeColor: Runnable = object: Runnable {
+            override fun run() {
+                progressBar.indeterminateTintList = ColorStateList.valueOf(Color.BLUE)
+                handler.postDelayed(this,2000)
+
             }
         }
-        alertDialogButton.setOnClickListener {
-            AlertDialog.Builder(this)
-                .setTitle("Remove last note")
-                .setMessage("Do you really want to remove the last writing? This operation cannot be undone!")
-                .setPositiveButton(android.R.string.ok) { _, _ ->
-                    val retrievedString = sharedPreferences.getString("KEY_DIARY_TEXT","")
-                    messages = retrievedString!!.split("\n\n").toMutableList()
-                    messages.removeAt(0)
-                    savedDiary.text = ""
-                    for (i in messages.indices) {
-                        if(i>0) {
-                            savedDiary.append("\n\n")
-                        }
-                        savedDiary.append(messages[i])
-                    }
-                    editor.putString("KEY_DIARY_TEXT",savedDiary.text.toString()).apply()
+        val changeColorState: Runnable = object: Runnable {
+            override fun run() {
+                progressBar.indeterminateTintList = ColorStateList.valueOf(Color.GREEN)
+                handler.postDelayed(this,2000)
+            }
+        }
+        val updateLight: Runnable = object : Runnable {
+            override fun run() {
+                handler.postDelayed(this, 1000)
+                val time = (System.currentTimeMillis() - timeOfStart) / 1000
+                val minutes = (time % 3600) / 60
+                val seconds = time % 60
+                val timeString = String.format("%02d:%02d", minutes, seconds)
+                textView.text = timeString
+                if(alertTime == time && alertTime!= 0.toLong()) {
+                    textView.setTextColor(Color.RED)
+                    val notificationManager1 = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    notificationManager1.notify(393939,notification)
 
+                }
+            }
+        }
+            startButton.setOnClickListener {
+                fun onStart() {
+                    super.onStart()
+                    progressBar.visibility = View.VISIBLE
+                    handler.postDelayed(updateLight, 0)
+                    handler.postDelayed(changeColor,0)
+                    handler.postDelayed(changeColorState,1000)
+                    settingsButton.isEnabled = false
+                }
+                if(!timerRunning) {
+                    timerRunning = true
+                    timeOfStart = System.currentTimeMillis()
+                    onStart()
+                }
+            }
+            resetButton.setOnClickListener {
+                fun onStop() {
+                    super.onStop()
+                    handler.removeCallbacks(updateLight)
+                    handler.removeCallbacks(changeColor)
+                }
+                onStop()
+                textView.text = "00:00"
+                progressBar.visibility = View.INVISIBLE
+                timerRunning = false
+                timeOfStart = System.currentTimeMillis()
+                textView.setTextColor(Color.GRAY)
+                settingsButton.isEnabled = true
+                alertTime = 100000
+
+            }
+        settingsButton.setOnClickListener {
+            val editText = EditText(this)
+            editText.inputType = InputType.TYPE_CLASS_PHONE
+            editText.id = R.id.upperLimitEditText
+            AlertDialog.Builder(this)
+                .setView(editText)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    if(editText.text.isNotBlank()) {
+                        alertTime = editText.text.toString().toLong()
+                    }
                 }
                 .setNegativeButton(android.R.string.cancel, null)
                 .show()
         }
+        }
 
-    }
+
 }
-
-
 
 
